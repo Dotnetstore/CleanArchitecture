44b61a3f2f90da05a1915ba2b8b76ee4356d2034
Hans Sjödin <hasse29@hotmail.com>
2023-12-09 11:18:32 +0100

    Added infrastructure objects

---
+++ src/Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Application.Common.Interfaces;
+using Infrastructure.Contexts;
+using Infrastructure.Persistence.Common;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection serviceCollection)
+    {
+        serviceCollection.AddDbContext<ApplicationDataContext>(q =>
+        {
+            q.UseSqlServer("Connectionstring");
+        });
+        
+        serviceCollection
+            .AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>))
+            .AddScoped<IUnitOfWork, UnitOfWork>();
+
+        return serviceCollection;
+    }
+}
