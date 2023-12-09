657ede79ec78ea3dd2249f1c1f5917c7dd499bca
Hans Sjödin <hasse29@hotmail.com>
2023-12-09 10:09:32 +0100

    Cleaned up Program.cs and added tests

---
+++ Tests/Presentation.Tests/Extensions/SetupApplicationExtensionsTests.cs
@@ -0,0 +1,67 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Builder;
+using Presentation.Extensions;
+
+namespace Presentation.Tests.Extensions;
+
+public class SetupApplicationExtensionsTests
+{
+    private WebApplicationBuilder _builder;
+
+    public SetupApplicationExtensionsTests()
+    {
+        _builder = WebApplication.CreateBuilder();
+    }
+    
+    [Fact]
+    public void AddServices_should_return_WebApplicationBuilder()
+    {
+        _builder.AddServices().Should().BeOfType<WebApplicationBuilder>();
+    }
+
+    [Fact]
+    public void Build_should_return_WebApplication()
+    {
+        _builder
+            .AddServices()
+            .BuildApplication()
+            .Should()
+            .BeOfType<WebApplication>();
+    }
+
+    [Fact]
+    public void AddSwagger_should_return_WebApplication()
+    {
+        _builder
+            .AddServices()
+            .BuildApplication()
+            .AddSwagger()
+            .Should()
+            .BeOfType<WebApplication>();
+    }
+
+    [Fact]
+    public void AddApplicationServices_should_return_WebApplication()
+    {
+        _builder
+            .AddServices()
+            .BuildApplication()
+            .AddSwagger()
+            .AddApplicationServices()
+            .Should()
+            .BeOfType<WebApplication>();
+    }
+
+    [Fact]
+    public void RunAppAsync_should_NotBeNull()
+    {
+        _builder
+            .AddServices()
+            .BuildApplication()
+            .AddSwagger()
+            .AddApplicationServices()
+            .RunAppAsync()
+            .Should()
+            .NotBeNull();
+    }
+}
---
+++ src/Presentation/Extensions/SetupApplicationExtensions.cs
@@ -0,0 +1,40 @@
+﻿namespace Presentation.Extensions;
+
+internal static class SetupApplicationExtensions
+{
+    internal static WebApplicationBuilder AddServices(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        return builder;
+    }
+
+    internal static WebApplication BuildApplication(this WebApplicationBuilder builder)
+    {
+        return builder.Build();
+    }
+
+    internal static WebApplication AddSwagger(this WebApplication app)
+    {
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        return app;
+    }
+
+    internal static WebApplication AddApplicationServices(this WebApplication app)
+    {
+        app.UseHttpsRedirection();
+
+        return app;
+    }
+
+    internal static Task RunAppAsync(this WebApplication app)
+    {
+        return app.RunAsync(new CancellationTokenSource().Token);
+    }
+}
