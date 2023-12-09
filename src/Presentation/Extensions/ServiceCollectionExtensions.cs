532f506f36b9a332221d642b9cc396b9bbead793
Hans Sjödin <hasse29@hotmail.com>
2023-12-09 12:14:23 +0100

    Added Presentation objects

---
+++ src/Presentation/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace Presentation.Extensions;
+
+internal static class ServiceCollectionExtensions
+{
+    internal static IServiceCollection AddPresentation(this IServiceCollection serviceCollection)
+    {
+        return serviceCollection;
+    }
+}
--- src/Presentation/Extensions/SetupApplicationExtensions.cs
+++ src/Presentation/Extensions/SetupApplicationExtensions.cs
@@ -1,13 +1,64 @@
-﻿namespace Presentation.Extensions;
+﻿using Application;
+using Application.Extensions;
+using Asp.Versioning;
+using Domain;
+using Infrastructure;
+using Infrastructure.Extensions;
+using Microsoft.Extensions.Options;
+using Presentation.Swagger;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace Presentation.Extensions;
 
 internal static class SetupApplicationExtensions
 {
     internal static WebApplicationBuilder AddServices(this WebApplicationBuilder builder)
     {
-        builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
+        builder
+            .Services
+            .AddApplication()
+            .AddInfrastructure()
+            .AddPresentation()
+            .AddMediator()
+            .AddVersioning()
+            .AddSwaggerService()
+            .AddEndpointsApiExplorer()
+            .AddSwaggerGen()
+            .AddControllers();
 
         return builder;
+    }
+
+    private static IServiceCollection AddMediator(this IServiceCollection serviceCollection)
+    {
+        serviceCollection.AddMediatR(q => q.RegisterServicesFromAssemblies(
+            typeof(IDomainAssemblyMarker).Assembly, 
+            typeof(IApplicationAssemblyMarker).Assembly, 
+            typeof(IInfrastructureAssemblyMarker).Assembly));
+
+        return serviceCollection;
+    }
+
+    private static IServiceCollection AddVersioning(this IServiceCollection serviceCollection)
+    {
+        serviceCollection.AddApiVersioning(x =>
+        {
+            x.DefaultApiVersion = new ApiVersion(1.0);
+            x.AssumeDefaultVersionWhenUnspecified = true;
+            x.ReportApiVersions = true;
+            x.ApiVersionReader = new MediaTypeApiVersionReader("api-version");
+        }).AddMvc().AddApiExplorer();
+    
+        return serviceCollection;
+    }
+
+    private static IServiceCollection AddSwaggerService(this IServiceCollection serviceCollection)
+    {
+        serviceCollection
+            .AddTransient<IConfigureOptions<SwaggerGenOptions>, ConfigureSwaggerOptions>()
+            .AddSwaggerGen(x => x.OperationFilter<SwaggerDefaultValues>());
+    
+        return serviceCollection;
     }
 
     internal static WebApplication BuildApplication(this WebApplicationBuilder builder)
@@ -20,7 +71,14 @@ internal static class SetupApplicationExtensions
         if (app.Environment.IsDevelopment())
         {
             app.UseSwagger();
-            app.UseSwaggerUI();
+            app.UseSwaggerUI(x =>
+            {
+                foreach (var description in app.DescribeApiVersions())
+                {
+                    x.SwaggerEndpoint( $"/swagger/{description.GroupName}/swagger.json",
+                        description.GroupName);
+                }
+            });
         }
 
         return app;
