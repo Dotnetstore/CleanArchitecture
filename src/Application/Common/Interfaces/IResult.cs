31a7450bd9b205de1b0f3ca411df6d027f03fb79
Hans Sjödin <hasse29@hotmail.com>
2023-12-12 19:03:06 +0100

    Added result

---
+++ src/Application/Common/Interfaces/IResult.cs
@@ -0,0 +1,16 @@
+﻿using Application.Common.Services;
+
+namespace Application.Common.Interfaces;
+
+public interface IResult
+{
+    /// <summary>
+    /// Gets the list of errors.
+    /// </summary>
+    List<Error>? Errors { get; }
+
+    /// <summary>
+    /// Gets a value indicating whether the state is error.
+    /// </summary>
+    bool IsError { get; }
+}
---
+++ src/Application/Common/Services/ErrorType.cs
@@ -0,0 +1,14 @@
+﻿namespace Application.Common.Services;
+
+/// <summary>
+/// Error types.
+/// </summary>
+public enum ErrorType
+{
+    Failure,
+    Unexpected,
+    Validation,
+    Conflict,
+    NotFound,
+    Unauthorized
+}
